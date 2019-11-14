6528ad53a69e31adf8aa580ad71aa4d01ae21523
Sergio Fernandez <sergiofv55@gmail.com>
2019-11-14 12:55:50 -0600

    Avance llamar ascensor

--- ProyectoAscensores/src/Ascensor.java
+++ ProyectoAscensores/src/Ascensor.java
@@ -1,9 +1,11 @@
 
 public class Ascensor {
 	private char identificador;
-	private String direccion; //Subir / bajar /quieto
+	private String direccionActual = "quieto";
 	private int pisoActual;
 	private int pisoDestino;
+	private int maxPasajeros;
+	private int cantidadPasajero;
 	
 	
 	public void subirUnPiso() {
@@ -12,5 +14,36 @@ public class Ascensor {
 	
 	public void bajarUnPiso() {
 		
+	}
+	
+	public void setMaxPasajeros(int maxPasajeros) {
+		this.maxPasajeros = maxPasajeros;
+	}
+	
+	public int getMaxPasajeros() {
+		return this.maxPasajeros;
+	}
+	
+	public void agregarPasajeros() {
+		++cantidadPasajero;
+	}
+	public void bajaPasajero() {
+		--cantidadPasajero;
+	}
+	
+	public String getDireccion() {
+		return direccionActual;
+	}
+	
+	public void setDireccion(String direccion) {
+		 direccionActual = direccion;
+	}
+	
+	public void setPisoActual(int piso) {
+		pisoActual = piso;
+	}
+	
+	public int getPisoActual() {
+		return pisoActual;
 	}
 }
--- ProyectoAscensores/src/Edificio.java
+++ ProyectoAscensores/src/Edificio.java
@@ -1,12 +1,34 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Edificio {
 	private int cantidadPisos;
 	private int cantidadAscensores;
+	private ArrayList<Ascensor> ascensores = new ArrayList<Ascensor>();
 	
 	public Edificio(int pisos, int ascensores) {
 		cantidadPisos = pisos;
 		cantidadAscensores = ascensores;
 	}
 	
+	public void llamaAscensor(int pisoActual, int pisoDestino) {
+		char mejorOpcion;
+		int pisoMasCercano;
+		Random random = new Random();
+		pisoMasCercano = random.nextInt(ascensores.size());
+		boolean pasajeroProximo;
+		for(Ascensor ascensor : ascensores) {
+			if((ascensor.getDireccion().equals("sube") && ascensor.getPisoActual() <= pisoActual)||
+			(ascensor.getDireccion().equals("baja")&& ascensor.getPisoActual() >= pisoActual) ||
+			(ascensor.getDireccion().equals("quieto")) ) {
+				pasajeroProximo = true;				
+			} else {
+				pasajeroProximo = false;
+			}
+		}
+	}
 	
+	public void addAscensor(Ascensor ascensor){
+		ascensores.add(ascensor);
+	}
 }
