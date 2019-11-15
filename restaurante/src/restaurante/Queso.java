ae422b7349ffbc80616e4e3486b87db68313549f
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2019-11-14 18:31:57 -0600

    Eliminada las clases de la idea ingrediente interface y subclases que implementan ese interface, ahora hay que restaurar los decoradores...

--- restaurante/src/restaurante/Queso.java
+++
@@ -1,9 +0,0 @@
-package restaurante;
-
-public class Queso extends IngredienteAdicional {
-	
-	public Queso() {
-		this.precio = 0.75;
-		this.descripcion = "Queso";
-	}
-}
--- restaurante/src/restaurante/Tomate.java
+++
@@ -1,9 +0,0 @@
-package restaurante;
-
-public class Tomate extends IngredienteAdicional {
-	
-	public Tomate() {
-		this.precio = 0.50;
-		this.descripcion = "Tomate";
-	}
-}
