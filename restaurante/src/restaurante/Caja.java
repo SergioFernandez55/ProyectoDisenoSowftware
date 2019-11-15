898ac4d06c6da8a3fb966426fb099f1b22c6acf6
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2019-11-14 23:00:23 -0600

    Clase caja con esqueleto de funcionalidades basicas descritas en el enunciado...

---
+++ restaurante/src/restaurante/Caja.java
@@ -0,0 +1,22 @@
+package restaurante;
+
+public class Caja {
+	
+	public void recibirOrden(){
+		// Pendiente implementar.
+	}
+	
+	public void despacharOdenAlChef(){
+		// Pendiente implementar.
+	}
+	
+	public void cobrar(){
+		// Pendiente implementar.
+	}
+	
+	public void generarFactura(){
+		// Fecha/Hora de la venta.
+		// Información del producto.
+		// Costo del producto.
+	}
+}
