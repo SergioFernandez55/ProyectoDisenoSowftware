bb9d19a5dfbaea411c26ebaaf027af88479dca0b
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2019-11-20 23:16:29 -0600

    Test general ahora procede hacer los test unitarios para probar las funcionalidades...

--- restaurante/src/restaurante/PruebasRestaurante.java
+++ restaurante/src/restaurante/PruebasRestaurante.java
@@ -11,28 +11,6 @@ public class PruebasRestaurante {
 	@Test
 	public void test() {
 		
-		//CajaRestaurante caja = new CajaRestaurante();
-		
-		//Producto italiano = new SandwichItaliano();
-		//Producto mexicano = new SandwichMexicano();
-		//Producto dobleQueso = new ConQueso(new ConQueso(new SandwichItaliano()));
-		//Producto tripleQueso = new ConQueso(new ConQueso(new ConQueso(new SandwichItaliano())));
-		
-		//caja.addProducto(italiano);
-		//caja.addProducto(mexicano);
-		//caja.addProducto(dobleQueso);
-		
-		//caja.imprimirFactura();
-		
-		//Memento antesTripleQueso = caja.creaEstado();
-		//caja.addProducto(tripleQueso);
-		
-		//caja.imprimirFactura();
-		
-		//caja.restauraEstado(antesTripleQueso);
-		
-		//caja.imprimirFactura();
-		
 		Caja caja = new CajaRestaurante();
 		Facturador facturador = new Facturador();
 		
@@ -51,8 +29,21 @@ public class PruebasRestaurante {
 		caja.addQuesoExtra();
 		caja.addQuesoExtra();
 		caja.addTomateExtra();
+		
+		caja.addNuevaLineaDePedido();
+		caja.addSandwichMexicano();
+		caja.addQuesoExtra();
+		caja.addTomateExtra();
+		
+		caja.addNuevaLineaDePedido();
+		caja.addSandwichItaliano();
+		caja.addTomateExtra();
+		
+		caja.addNuevaLineaDePedido();
+		caja.addSandwichMexicano();
+		
 		caja.finalizarOrden();
-
+		
 		facturador.imprimir(caja.getOrden());
 	}
 }
