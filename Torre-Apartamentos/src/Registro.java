1e24f90f2ba4fc32e4e0e558e35dfe3e2ec0f74e
Dennis Solano <b59304@ecci.ucr.ac.cr@ECCI393265L4-15.ecci.ucr.ac.cr>
2019-11-27 19:10:00 -0600

    Eliminado el atributo tiempo... ahora se crea una instancia cada vez que se va a agregar...

--- Torre-Apartamentos/src/Registro.java
+++ Torre-Apartamentos/src/Registro.java
@@ -4,10 +4,9 @@ import java.util.List;
 public class Registro {
 	
 	private List<String> registro = new ArrayList<>();
-	private Tiempo tiempo;
 	
 	public void agregar(String tarjeta, String tipoAcceso){
-		this.registro.add(tarjeta + " " + this.tiempo.getFecha() + " " + tipoAcceso);
+		this.registro.add(tarjeta + " " + new Tiempo().getFecha() + " " + tipoAcceso);
 	}
 	
 	public List<String> obtener(){
